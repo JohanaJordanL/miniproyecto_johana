4b1cac721f0459f6944bf2265155665776d0b159
JohanaJordanL <83962619+JohanaJordanL@users.noreply.github.com>
2021-06-15 10:30:59 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -1,1 +1,16 @@
+<?php
 
+namespace App\Controllers;
+use App\models\datosmodel;
+class miniproyecto extends basecontroller
+{
+  public function index()
+  {
+    $model = new datosmodel();
+    $datos = $model->listar();
+    $data = [
+      "datos" => $datos 
+      ]
+      return view('listado' ,$data);
+  }
+}
