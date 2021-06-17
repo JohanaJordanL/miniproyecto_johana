e6d6f9af6300259becbf6c71e8efadcdd9250aa8
JohanaJordanL <83962619+JohanaJordanL@users.noreply.github.com>
2021-06-17 11:13:35 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -10,7 +10,62 @@ class Miniproyecto extends Basecontroller
     $datos = $model->listar();
     $data = [
       "datos" => $datos 
-      ]
+      ];
       return view('listado' ,$data);
   }
 }
+public function obtenerDatos($id){
+  $gModel = new generalModel();
+  $data = ["id" => $id];
+  $respuesta = $gmodel->obtenerInformacion($data);
+  
+  $datos = ["datos" => $respuesta];
+  return view('actualizar',$datos);
+  
+}
+
+public fuction insertar(){
+  $gmodel = new generalModel();
+  $data =[
+    "nombre" => $POST['nombre'],
+    "a_paterno" => $_POST['apaterno'],
+    "aa_materno => $_POST['amaterno'],
+    
+    ];
+    $respuesta = $gModel->insertar($data);m
+    if ($respuesta > 0){
+    return redirect()->to(base_url('/index.php'))->with('mensaje','0');
+    }else{
+    return redirect()->to(base_url('/index.php'))->with('mensaje','1');
+    }
+    }
+    
+    public function actualizar(){
+    $gModel = new generalModel();
+    $data = [
+    "nombre" => $_POST['nombre'],
+    "a_paterno" => $_POST['apaterno'],
+    "a_materno" => $_POST['amaterno'],
+    ];
+    $id = ["id" => $_POST['id']];
+    $respuesta =  gModel->actualizar($data,$id);
+    
+    if ($respuesta){
+    return redirect()->to(base_url('/index.php'))->with('mensaje','2');
+    }else{
+    return redirect()->to(base_url('/index.php))->with('mensaje','3');
+    }
+    }
+    public function eliminar($idPersona){
+    $gModel = new GeneralModel();
+    $id = {"id" => $idPersona];
+    $respuesta = $gModel->eliminar($id);
+    
+    if($respuesta){
+    return redirect()->to(base_url('/index.php'))->with('mensaje','4');
+    }else{
+    return redirect()->to(base_url('/index.php))->with('mensaje','5');
+    }
+    }
+    }
+    
