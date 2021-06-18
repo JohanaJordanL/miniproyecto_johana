eeb2d90853544ed41f17e20994110353548b3122
JohanaJordanL <83962619+JohanaJordanL@users.noreply.github.com>
2021-06-18 18:09:29 -0500

    Update Miniproyecto.php

--- codeingniter/app/Controllers/Miniproyecto.php
+++ codeingniter/app/Controllers/Miniproyecto.php
@@ -1,71 +1,80 @@
 <?php
+	
 
-namespace App\Controllers;
-use App\Models\Datosmodel;
-class Miniproyecto extends Basecontroller
-{
-  public function index()
-  {
-    $model = new datosmodel();
-    $datos = $model->listar();
-    $data = [
-      "datos" => $datos 
-      ];
-      return view('listado' ,$data);
-  }
-}
-public function obtenerDatos($id){
-  $gModel = new DatosModel();
-  $data = ["id" => $id];
-  $respuesta = $gmodel->obtenerInformacion($data);
-  
-  $datos = ["datos" => $respuesta];
-  return view('actualizar',$datos);
-  
-}
+	namespace App\Controllers;
+	use App\Models\DatosModel;
+	class Miniproyecto extends BaseController
+	{
+		public function index()
+		{
+			$model = new DatosModel();
+			$mensaje = session('mensaje');
+			$datos = $model->listarTodo();
+			$data = [
+	                     "datos" => $datos,
+				"mensaje" => $mensaje
+			];
+			return view('listado',$data);
+		}
+	
 
-public fuction insertar(){
-  $gmodel = new DatosModel();
-  $data =[
-    "nombre" => $POST['nombre'],
-    "a_paterno" => $_POST['apaterno'],
-    "aa_materno => $_POST['amaterno'],
-    
-    ];
-    $respuesta = $gModel->insertar($data);m
-    if ($respuesta > 0){
-    return redirect()->to(base_url('/index.php'))->with('mensaje','0');
-    }else{
-    return redirect()->to(base_url('/index.php'))->with('mensaje','1');
-    }
-    }
-    
-    public function actualizar(){
-    $gModel = new DatosModel();
-    $data = [
-    "nombre" => $_POST['nombre'],
-    "a_paterno" => $_POST['apaterno'],
-    "a_materno" => $_POST['amaterno'],
-    ];
-    $id = ["id" => $_POST['id']];
-    $respuesta =  gModel->actualizar($data,$id);
-    
-    if ($respuesta){
-    return redirect()->to(base_url('/index.php'))->with('mensaje','2');
-    }else{
-    return redirect()->to(base_url('/index.php))->with('mensaje','3');
-    }
-    }
-    public function eliminar($idPersona){
-    $gModel = new DatosModel();
-    $id = {"id" => $idPersona];
-    $respuesta = $gModel->eliminar($id);
-    
-    if($respuesta){
-    return redirect()->to(base_url('/index.php'))->with('mensaje','4');
-    }else{
-    return redirect()->to(base_url('/index.php))->with('mensaje','5');
-    }
-    }
-    }
-    
+	        public function obtenerDatos($id) {
+			$model = new DatosModel();
+			$data = ["id" => $id];
+			$respuesta = $model ->obtenerInformacion($data);
+			
+			$datos = ["datos" => $respuesta];
+			return view('actualizar', $datos);
+			
+		}
+	        public function insertar(){
+			$model = new DatosModel();
+			$data = [
+				"nombre" => $_POST ['nombre'],
+				"apellido_paterno" => $_POST ['apaterno'],
+				"apellido_materno" => $_POST ['amaterno'],
+			];
+			$respuesta = $model ->insertar($data);
+			
+			if($respuesta > 0){
+				return redirect()->to(base_url('/index.php'))->with('mensaje','0');
+			}else{
+				return redirect()->to(base_url('/index.php'))->with('mensaje','1');
+			}
+		}
+	       
+	
+
+
+
+	        public function actualizar(){
+			$model = new DatosModel();
+			$data = [
+				"nombre" => $_POST ['nombre'],
+				"apellido_paterno" => $_POST ['apaterno'],
+				"apellido_materno" => $_POST ['amaterno'],
+				
+				
+			];
+			$id = ["id" => $_POST['id']];
+			$respuesta = $model->actualizar($data,$id);
+			
+			if($respuesta){
+				return redirect()->to(base_url('/index.php'))->with('mensaje','2');
+			}else{ 
+				return redirect()->to(base_url('/index.php'))->with('mensaje','3');
+			}
+		}
+	        public function eliminar($idPersona){
+			$model = new DatosModel();
+			$id = [ "id" => $idPersona];
+			$respuesta = $model->eliminar($id);
+			
+			if($respuesta){
+				return redirect()->to(base_url('/index.php'))->with('mensaje','4');
+			}else{
+				return redirect()->to(base_url('/index.php'))->with('mensaje','5');
+			}
+		}
+	}
+	                         
